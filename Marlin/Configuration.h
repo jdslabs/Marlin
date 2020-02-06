3eb786845616370266d414999bdbec26d8056ed3
jdslabs <contact@jdslabs.com>
2020-02-05 22:43:49 -0600

    One more note

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1083,7 +1083,7 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
+#define X_MIN_POS 0     // JDS: Is this correct?
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
